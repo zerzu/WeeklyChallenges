23d0a1c8ca417175c9c99d761c729291d596d10c
Eric Garner <ericgarner81@gmail.com>
2023-11-16 17:23:44 -0600

    completed set 3

--- ChallengesWithTestsMark8.Tests/obj/Debug/net6.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net6.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae3471b6d227ebaba459e94746ec76a5a832aabb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +9,111 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            bool valBool = false;
+            foreach (var b in vals)
+            {
+                if (b == false)
+                {
+                    valBool = true;
+                    break;
+                }
+
+            }
+
+            return valBool;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            bool valBool = false;
+            int sum = 0;
+
+            if (numbers == null || !numbers.Any())
+            {
+                return valBool;
+            }
+
+            foreach (var number in numbers)
+            {
+                if (number % 2 != 0)
+                {
+                    sum += number;
+                }
+            }
+
+            if (sum % 2 != 0)
+            {
+                valBool = true;
+            }
+
+            return valBool;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            Regex regex = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$");
+
+            return regex.IsMatch(password);
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+
+            }
+
+            else
+            {
+                return dividend / divisor;
+
+            }
         }
+
+
+
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var numList = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                if ((i % 2) != 0)
+                {
+                    numList.Add(i);
+                }
+            }
+
+            return numList.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != null)
+                {
+                    words[i] = words[i].ToUpper();
+                }
+            }
         }
     }
+    
 }
